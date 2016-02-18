a6a10f7688c973c603f882e5cd04e5794e828fd5
Kirill Erofeev <krllerof@gmail.com>
2016-02-18 05:08:50 +0300

    log, processing exceptions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,7 @@ import java.util.Scanner;
  */
 public class Main  {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);//debug
-        String r = "Tf";
-        System.out.println(ItemsDaoFileBasedImpl.class.getName());
-
+        
         }
     }
 
